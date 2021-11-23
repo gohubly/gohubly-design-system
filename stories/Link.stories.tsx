a234a05c070607a66495a3c14d6c2963784143bf
Lucas Tiberio <ltiberio55@gmail.com>
2021-11-23 15:56:42 -0300

    added onColor example

--- stories/Link.stories.tsx
+++ stories/Link.stories.tsx
@@ -6,4 +6,5 @@ import { Link } from '../src';
 storiesOf('Link', module)
   .add('Default', () => <Link>Action</Link>)
   .add('With onClick', () => <Link onClick={() => undefined}>Action</Link>)
+  .add('With onColor', () => <Link onColor onClick={() => undefined}>Action</Link>)
   .add('Disabled', () => <Link onClick={() => undefined} disabled>Action Disabled</Link>);
