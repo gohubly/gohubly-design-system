90010613c4c7f1c706d44754631b7e7c4412d3ad
Filipe Neves <filipe.neves@gohubly.com>
2022-01-17 09:27:55 -0300

    adjusting spinner sizing

--- src/components/Spinner/spinner.interface.ts
+++ src/components/Spinner/spinner.interface.ts
@@ -1,4 +1,5 @@
 export interface iSpinner {
   onColor?: boolean
   size?: string
+  className?: string
 }
--- src/components/Spinner/spinner.style.ts
+++ src/components/Spinner/spinner.style.ts
@@ -6,7 +6,7 @@ const loadingSpinAnimation = keyframes`
   to {transform: rotate(360deg)}
 `
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.span`
   width: 100%;
   height: 100%;
   display: flex;
@@ -15,9 +15,11 @@ export const Wrapper = styled.div`
   line-height: 0;
 `
 
-export const SpinnerStyled = styled(Loading)<{ onColor: boolean }>`
+export const SpinnerStyled = styled(Loading)<{ onColor: boolean, size?: string }>`
   transition-property: transform;
   animation: ${loadingSpinAnimation} 1s linear infinite;
+  width: ${({ size }) => size};
+  height: ${({ size }) => size};
 
   path {
     fill: ${({ onColor, theme }) => onColor ? theme.colors.white : theme.colors.primary};
