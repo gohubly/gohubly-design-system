140f61a05183b28eab1cb24e4db3107d1a7234f1
Lucas Eduardo <lucased78@hotmail.com>
2022-08-05 12:47:27 -0300

    fix: highlight label in accordion

--- src/components/Accordion/accordion.component.tsx
+++ src/components/Accordion/accordion.component.tsx
@@ -8,6 +8,18 @@ import * as css from './accordion.style'
 export const Accordion: React.FC<iAccordion> = ({ items, width = "400px", fluid, highlightLabel, action }) => {
   const [openedAccordion, setOpenedAccordion] = useState<string | undefined>(undefined);
 
+  const renderLabelWithHighlight = (label: string) => {
+    const splitted = label.split(" ");
+    
+    return splitted.map((label, index) => (
+      <Typography size='XS' color={index < splitted.length - 2 && highlightLabel ? 'primary' : 'neutralLow'}>
+        <css.Label clear={index > 0}>
+          { label }
+        </css.Label>
+      </Typography>
+    ))
+  }
+
   if (width === "100%") throw new Error("use fluid intead of setting width to 100%");
 
   return (
@@ -17,16 +29,10 @@ export const Accordion: React.FC<iAccordion> = ({ items, width = "400px", fluid,
           <css.AccordionItemContainer>
             <css.Item onClick={() => setOpenedAccordion(value === openedAccordion ? undefined : value)}>
               <css.LeftContent>
-                <Icon iconId={value === openedAccordion ? 'chevronUp' : 'chevronDown'} size='MD' />
+                {items.length  && <Icon iconId={value === openedAccordion ? 'chevronUp' : 'chevronDown'} size='MD' />}
 
                 {
-                  label.split(" ").map((label, index) => (
-                    <Typography size='XS' color={index === 0 && highlightLabel ? 'primary' : 'neutralLow'}>
-                      <css.Label clear={index > 0}>
-                        { label }
-                      </css.Label>
-                    </Typography>
-                  ))
+                  renderLabelWithHighlight(label)
                 }
               </css.LeftContent>
 
