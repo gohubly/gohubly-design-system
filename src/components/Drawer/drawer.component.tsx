3aa8f15848f86c7893478e83bd21c77ed67f690f
Lucas Eduardo <lucased78@hotmail.com>
2022-12-06 15:51:16 -0300

    fix: hide body scroll when Drawer is open

--- src/components/Drawer/drawer.component.tsx
+++ src/components/Drawer/drawer.component.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from '@storybook/addons'
 import React from 'react'
 import * as css from './drawer.styles'
 import { iDrawerProps } from './drawer.types'
@@ -9,10 +10,20 @@ export const Drawer: React.FC<iDrawerProps> = ({
   drawerRef,
   overlayRef,
   ...props
-}) => (
-  <css.Overlay show={show} zIndex={zIndex} ref={overlayRef}>
-    <css.Content {...props} zIndex={zIndex} ref={drawerRef}>
-      { children }
-    </css.Content>
-  </css.Overlay>
-)
+}) => {
+  useEffect(() => {
+    if (show) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+  }, [show])
+
+ return (
+    <css.Overlay show={show} zIndex={zIndex} ref={overlayRef}>
+      <css.Content {...props} zIndex={zIndex} ref={drawerRef}>
+        { children }
+      </css.Content>
+    </css.Overlay>
+  )
+}
