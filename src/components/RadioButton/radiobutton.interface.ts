250a6a626e3f8759083bf85d583821d28168c606
Nathália Mayworm <nathalia.sa@b8one.com>
2023-01-31 17:09:03 -0300

    feat: adding underscore on radioButton

--- src/components/RadioButton/radiobutton.interface.ts
+++ src/components/RadioButton/radiobutton.interface.ts
@@ -1,32 +1,36 @@
+export type IPaddingOptions = "SM" | "MD" | "LG" | "XL" | "XXL";
 export interface iRadio {
-    label?: string;
-    name: string;
-    value: any;
-    readOnly?: boolean;
-    onChange?: (value: any) => void;
-    disabled?: boolean;
-    onColor?: boolean;
-    options?: iRadioOption[];
-  }
+  label?: string;
+  name: string;
+  value: any;
+  readOnly?: boolean;
+  onChange?: (value: any) => void;
+  disabled?: boolean;
+  onColor?: boolean;
+  options?: iRadioOption[];
+  underlined?: boolean;
+  paddingOptions?: IPaddingOptions;
+}
 
-  export interface iRadioOption {
-    title?: string,
-    value: string,
-    child?: JSX.Element | React.ReactChild | any;
-  }
-  
-  export interface ILabelRadio {
-    disabled?: boolean;
-    onColor?: boolean;
-    options?: iRadioOption[];
-  }
-  
-  export interface IBoxRadio {
-    ref: any;
-  }
+export interface iRadioOption {
+  title?: string;
+  value: string;
+  child?: JSX.Element | React.ReactChild | any;
+}
 
-  export interface IMarkRadio {
-    disabled?: boolean;
-    onColor?: boolean;
-  }
-  
+export interface ILabelRadio {
+  disabled?: boolean;
+  onColor?: boolean;
+  options?: iRadioOption[];
+  underlined?: boolean;
+  paddingOptions?: IPaddingOptions;
+}
+
+export interface IBoxRadio {
+  ref: any;
+}
+
+export interface IMarkRadio {
+  disabled?: boolean;
+  onColor?: boolean;
+}
