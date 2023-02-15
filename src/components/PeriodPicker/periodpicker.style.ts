e95f1627c8922bf0242b6e9c427f16f17e82967e
Nathália Mayworm <nathalia.sa@b8one.com>
2023-02-15 16:16:29 -0300

    feat: adding new layout in periodpiker

--- src/components/PeriodPicker/periodpicker.style.ts
+++ src/components/PeriodPicker/periodpicker.style.ts
@@ -1,19 +1,20 @@
-import styled, { css } from 'styled-components'
+import styled, { css } from "styled-components";
 import { defaultCss } from "../../theme/defaultCss";
 
-export const Container = styled.div`
+export const Container = styled.div<{ width?: string }>`
   ${defaultCss};
 
-  height: 401px;
+  height: 100%;
   display: flex;
   flex-direction: column;
+  width: ${({ width }) => width ?? "100%"}
   background-color: ${({ theme }) => theme.colors.neutralHigh};
 
   border: 1px solid ${({ theme }) => theme.colors.neutralHighLight};
   border-radius: ${({ theme }) => theme.borderRadius.SM};
 
   position: relative;
-`
+`;
 
 const ControlMonthButtonDefault = styled.div`
   position: absolute;
@@ -21,7 +22,7 @@ const ControlMonthButtonDefault = styled.div`
   top: 18px;
   z-index: 1;
   cursor: pointer;
-`
+`;
 
 export const PreviousButtonContainer = styled(ControlMonthButtonDefault)`
   display: flex;
@@ -40,7 +41,7 @@ export const PreviousButtonContainer = styled(ControlMonthButtonDefault)`
       stroke: ${({ theme }) => theme.colors.neutralLow};
     }
   }
-`
+`;
 
 export const NextButtonContainer = styled(ControlMonthButtonDefault)`
   display: flex;
@@ -59,21 +60,22 @@ export const NextButtonContainer = styled(ControlMonthButtonDefault)`
       stroke: ${({ theme }) => theme.colors.neutralLow};
     }
   }
-`
+`;
 
 export const MonthsContainer = styled.div<{ count?: number }>`
   display: inline-grid;
   grid-template-columns: ${({ count = 3 }) => `repeat(${count}, 1fr)`};
   gap: 24px;
-  margin: 24px;
+  margin: 32px 24px;
   flex: 1;
-`
+`;
 
 export const Month = styled.div`
+  align-items: center;
   display: flex;
   flex-direction: column;
   gap: 24px;
-`
+`;
 
 export const MonthApresentation = styled.div`
   font-size: 14px;
@@ -83,39 +85,48 @@ export const MonthApresentation = styled.div`
   text-transform: capitalize;
 
   user-select: none;
-`
+`;
 
 export const MonthWeeks = styled.div`
   display: flex;
   flex-direction: column;
   gap: 4px;
-`
+`;
 
 export const Week = styled.div`
   display: grid;
   grid-template-columns: repeat(7, 41px);
   justify-content: center;
-`
+`;
 
 const DayDefault = styled.div`
   height: 28px;
-  font-size: 14px;
+  font-size: 12px;
   line-height: 16px;
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
 export const DayName = styled(DayDefault)`
   color: ${({ theme }) => theme.colors.neutralLowLight};
 
   user-select: none;
-`
+`;
 
-export const DayNumber = styled(DayDefault)<{ isBetween?: boolean, isSelected?: boolean, isInitialDate?: boolean, isTotalSelected?: boolean, disabled?: boolean}>`
-  color: ${({ theme, disabled }) => disabled ? theme.colors.neutralLowLight : theme.colors.neutralLow};
+export const DayNumber = styled(DayDefault)<{
+  isBetween?: boolean;
+  isSelected?: boolean;
+  isInitialDate?: boolean;
+  isTotalSelected?: boolean;
+  disabled?: boolean;
+}>`
+  color: ${({ theme, disabled }) =>
+    disabled ? theme.colors.neutralLowLight : theme.colors.neutralLow};
   position: relative;
-  background-color: ${({ isBetween, isSelected, theme }) => isBetween ? theme.colors.primaryLightest : 'transparent'};
+  background-color: ${({ isBetween, isSelected, theme }) =>
+    isBetween ? theme.colors.primaryLightest : "transparent"};
+
   cursor: pointer;
 
   span {
@@ -123,57 +134,60 @@ export const DayNumber = styled(DayDefault)<{ isBetween?: boolean, isSelected?: 
   }
 
   ::before {
-    content: '';
+    content: "";
     height: 28px;
     width: 28px;
     position: absolute;
-    border-radius: 50%;
+    border-radius: 4px;
   }
 
-  ${({ disabled }) => disabled && css`
-    cursor: not-allowed;
-  `}
+  ${({ disabled }) =>
+    disabled &&
+    css`
+      cursor: not-allowed;
+    `}
 
-  ${({ isSelected, theme, isInitialDate, isTotalSelected }) => isSelected && css`
-    color: ${({ theme }) => theme.colors.neutralHigh};
-    background: ${isTotalSelected && `linear-gradient(${isInitialDate ? '90deg' : '270deg'}, transparent 0%, transparent 50%, ${theme.colors.primaryLightest} 20%)`} ;
+  ${({ isSelected, theme, isInitialDate, isTotalSelected }) =>
+    isSelected &&
+    css`
+      color: ${({ theme }) => theme.colors.neutralHigh};
+      background: ${isTotalSelected &&
+      `linear-gradient(${
+        isInitialDate ? "90deg" : "270deg"
+      }, transparent 0%, transparent 50%, ${
+        theme.colors.primaryLightest
+      } 20%)`};
 
-    ::before {
-      background: ${({ theme }) => theme.colors.primary};
-    }
-  `}
+      ::before {
+        background: ${({ theme }) => theme.colors.primaryMedium};
+      }
+    `}
 
-  ${({ isBetween, theme }) => isBetween && css`
-    color: ${theme.colors.primary};
-  `}
-
-  :nth-child(7n+1) {
-    border-radius: 120px 0 0 120px;
-  }
-
-  :nth-child(7n) {
-    border-radius: 0 120px 120px 0;
-  }
-`
+  ${({ isBetween, theme }) =>
+    isBetween &&
+    css`
+      color: ${theme.colors.primaryMedium};
+    `}
+`;
 
 export const Footer = styled.div`
   display: flex;
   justify-content: flex-end;
   align-items: center;
-  
+
   padding: 16px;
-  
+
   border-top: 1px solid ${({ theme }) => theme.colors.neutralHighLight};
-`
+`;
 
 export const HelperContainer = styled.div`
   margin-right: auto;
 
   user-select: none;
-`
+`;
 
 export const PeriodCallout = styled.div`
   margin-right: auto;
   font-size: 12px;
   color: ${({ theme }) => theme.colors.neutralLow};
-`
+`;
