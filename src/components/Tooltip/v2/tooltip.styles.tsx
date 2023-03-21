6d01cebb735bd2aa3144fe845310d2f62ee551d2
Nathália Mayworm <nathalia.sa@b8one.com>
2023-03-21 17:35:51 -0300

    fix: fixing merge

--- src/components/Tooltip/v2/tooltip.styles.tsx
+++ src/components/Tooltip/v2/tooltip.styles.tsx
@@ -13,21 +13,16 @@ const FONT_SIZE_BY_SIZE: Record<iTooltipSizes, string> = {
 
 export const Container = styled.div<iTooltip>`
   .react-tooltip {
-    box-sizing: border-box;
-
-    border-radius: 4px !important;
-
-    font-size: ${({ size }) => FONT_SIZE_BY_SIZE[size as iTooltipSizes]};
-
-    padding: ${({ size }) => PADDING_BY_SIZE[size as iTooltipSizes]};
-
-    background: ${({ theme }) => theme.colors.neutralLowDark} !important;
-
-    color: ${({ theme }) => theme.colors.neutralHigh};
-
     width: max-content;
-
     max-width: ${({ maxWidth }) => maxWidth ?? '100%'};
+    box-sizing: border-box;
+    
+    font-size: ${({ size }) => FONT_SIZE_BY_SIZE[size as iTooltipSizes]};
+    padding: ${({ size }) => PADDING_BY_SIZE[size as iTooltipSizes]};
+    
+    border-radius: 4px !important;
+    background: ${({ theme }) => theme.colors.neutralLowDark} !important;
+    color: ${({ theme }) => theme.colors.neutralHigh};
 
     z-indez: 3;
   }
